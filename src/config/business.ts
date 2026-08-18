a73a5440e23f55614ab0fc9406e05fc7fe3a62c2
Swathi V <swathiv0604@gmail.com>
2026-08-18 21:42:12 +0530

    business.ts

--- src/config/business.ts
+++ src/config/business.ts
@@ -1,7 +1,7 @@
 export const BUSINESS_CONFIG = {
   name: "BookYourService",
   whatsappNumber: "918310421121", // country code + number, no +, no spaces, no dashes
-  upiId: "7892482733@ibl",
+  upiId: "8310421121@ybl",
   upiPayeeName: "BookYourService",
   city: "Bengaluru, Karnataka, India",
   // Paste the official Instagram profile URL here to show Instagram links site-wide.
